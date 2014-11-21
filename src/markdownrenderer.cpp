9bd0804cb087925a929223b6f1c85e4c62ade841
Charles <char101@ui.ac.id>
2014-11-21 15:06:28 +0700

    Enable all hoedown extensions

--- src/markdownrenderer.cpp
+++ src/markdownrenderer.cpp
@@ -2,11 +2,17 @@
 
 #include <QDebug>
 
+static const int html_flags = HOEDOWN_HTML_USE_XHTML;
+static const int enabled_exts = HOEDOWN_EXT_TABLES | HOEDOWN_EXT_FENCED_CODE | HOEDOWN_EXT_FOOTNOTES |
+                                HOEDOWN_EXT_AUTOLINK | HOEDOWN_EXT_STRIKETHROUGH | HOEDOWN_EXT_UNDERLINE |
+                                HOEDOWN_EXT_HIGHLIGHT | HOEDOWN_EXT_QUOTE | HOEDOWN_EXT_SUPERSCRIPT | HOEDOWN_EXT_MATH;
+
 MarkdownRenderer::MarkdownRenderer()
 {
+
     // if nesting = 0, no text is being renderered
-    renderer =  hoedown_html_renderer_new(static_cast<hoedown_html_flags>(0), 16); // render flags, nesting (toc depth)
-    document = hoedown_document_new(renderer, static_cast<hoedown_extensions>(0), 16); // extensions, nesting
+    renderer = hoedown_html_renderer_new(static_cast<hoedown_html_flags>(html_flags), 16); // render flags, nesting (toc depth)
+    document = hoedown_document_new(renderer, static_cast<hoedown_extensions>(enabled_exts), 16); // extensions, nesting
 }
 
 MarkdownRenderer::~MarkdownRenderer()
